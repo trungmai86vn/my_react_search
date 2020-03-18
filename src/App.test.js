811792edbb8ee5e2d48731b2e8ff4e867dee4848
trungmai86vn <57646108+trungmai86vn@users.noreply.github.com>
2020-03-18 20:17:32 +1100

    Cleaning

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Live Search: React Application', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appTitle = getByText(/Live Search: React Application/i);
+  expect(appTitle).toBeInTheDocument();
 });
